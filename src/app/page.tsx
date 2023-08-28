df64ad19a75f73456e0326ab363d70c92d3e0bca
Kevan Herrera <Kevanpereira12>
2023-08-28 09:57:47 -0600

    principal

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import React from 'react';
+import Link from 'next/link';
+import './globals.css';
+
+export default function Home() {
+  return (
+    
+    <html>
+    <head>
+        
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a>Kafitness</a></li>
+                    <li><a href="#horario">Horario</a></li>
+                    <li><a href="#ejercicios">Ejercicios</a></li>
+                    <li><a href="#Galeria">Galería</a></li>
+                </ul>
+            </nav>
+        </header>
+    
+        <main>
+            
+        </main>
+    
+        <footer>
+            <p>&copy; Kafitness 2023 </p>
+        </footer>
+    </body>
+    </html>
+  );
+};
